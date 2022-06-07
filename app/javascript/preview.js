51b6b36278704994f0486561526c1c27cace3034
mijinko-j <mijinko415@gmail.com>
2022-06-07 18:27:23 +0900

    画像プレビュー機能実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new-member');
+  const previewList = document.getElementById('previews');
+  if (!postForm) return null;
+
+  const fileField = document.querySelector('input[type="file"][name="member[image]"]');
+  fileField.addEventListener('change', function(e){
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    console.log("input要素で値の変化が起きました");
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    console.log(blob);
+
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
