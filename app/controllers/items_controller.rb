74deb682c4de647b1b61e2631bcc87f9c23f2dc9
mijinko-j <mijinko415@gmail.com>
2022-06-11 16:49:16 +0900

    おてつだい管理機能実装

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,58 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.includes(:user)
+    
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def edit
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,7 @@
-class Member < ApplicationRecord
+class Item < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
 
   validates :name, presence: true
-  validates :fixed, presence: true,
+  validates :price, presence: true,
   numericality: { only_integer: true, less_than_or_equal_to: 9_999_999, allow_blank: true }, format: { with: /\A[0-9]+\z/, allow_blank: true }
-  validates :color, presence: true
 end
