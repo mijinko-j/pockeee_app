abffa8760cd82739dad18f574b83842ca7a025cd
mijinko-j <mijinko415@gmail.com>
2022-06-20 18:57:05 +0900

    comment単体テストコード

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :member
 
-  validates :comment, presence: true
+  validates :comment,        presence: true
+  validates :comment_member, presence: true
 end
